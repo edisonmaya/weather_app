2101900e838d9b415c487395472268229c0eea2f
edisonmaya <ee_mayahuayamave2@hotmail.com>
2023-10-02 19:02:43 -0500

    media query

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -24,16 +24,16 @@ const WeatherContainer = ({ weather, dark, setDark }) => {
     return (
         <section className="grid gap-8 text-center ">
             <Button_slider dark={dark} setDark={setDark} ></Button_slider>
-            <div className='grid gap-5'>
             <h3 className='text-xl font-semibold'>{weather.name + ' '},{' ' + weather.sys.country}</h3>
-            <article className="bg-slate-500/50 rounded-[38px] grid grid-cols-2 gap-2 py-6 items-center justify-center">
+            <div className='grid gap-5 sm:grid-cols-[1fr_auto]' >
+            <article className="bg-slate-500/50 rounded-[38px] grid grid-cols-2 gap-2 py-6 items-center justify-center ">
                 <h4 className='col-span-2 text-2xl capitalize'>{weather.weather[0].description}</h4>
                 <span className='text-4xl' >{changeUnitTemp(weather.main.temp)}</span>
                 <picture className='grid justify-center items-center'>
                     <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt={weather.weather[0].description} />
                 </picture>
             </article>
-            <article className="grid grid-cols-3 divide-x justify-center items-center bg-slate-500/50 rounded-2xl h-16 w-full gap-1 p1">
+            <article className="grid grid-cols-3  divide-x py-4 justify-center items-center bg-slate-500/50 rounded-2xl min-h-max w-full gap-1 sm:grid-cols-1  sm:divide-x-0 sm:divide-y-2">
                 <WeatherStat icon={"/wind"} unit={" m/s"} value={weather.wind.speed} dark={dark}></WeatherStat>
                 <WeatherStat icon={"/humidity"} unit={" %"} value={weather.main.humidity} dark={dark}></WeatherStat>
                 <WeatherStat icon={"/pressure"} unit={" hPa"} value={weather.main.pressure} dark={dark}></WeatherStat>
@@ -41,9 +41,9 @@ const WeatherContainer = ({ weather, dark, setDark }) => {
             </div>
             <div className='flex justify-center items-center'>
             {dark === true ?
-                    <button onClick={handleChangeUnit} className='flex justify-center items-center w-36 h-9 text-white bg-blue-500 rounded-2xl py-4 '>Change to °C / °F</button>
+                    <button onClick={handleChangeUnit} className='flex justify-center items-center w-36 h-9 text-white bg-blue-500 rounded-2xl py-4 font-semibold'>Change to °C / °F</button>
                     :
-                    <button onClick={handleChangeUnit} className='flex justify-center items-center w-36 h-9 bg-white text-blue-500 rounded-2xl py-4 '>Change to °C / °F</button>
+                    <button onClick={handleChangeUnit} className='flex justify-center items-center w-36 h-9 bg-white text-blue-500 rounded-2xl py-4 font-semibold'>Change to °C / °F</button>
                 }    
             </div>
         </section>
--- src/components/WeatherStat.jsx
+++ src/components/WeatherStat.jsx
@@ -3,7 +3,7 @@ const WeatherStat = ({icon, unit, value, dark}) => {
     const iconDark= icon+'_dark.svg'
     const iconLight= icon+'_light.svg'
     return (
-        <div className="flex gap-1 items-center justify-center ">
+        <div className="flex gap-1 items-center justify-center sm:p-[24px] font-semibold">
                 {dark===true?
                 <>
                 <img className="h-5 w-5 " src={iconDark} />
