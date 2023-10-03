39a4de3d6db3ac228eefe9abf132f0e6f32ed5d1
edisonmaya <ee_mayahuayamave2@hotmail.com>
2023-10-02 19:24:34 -0500

    alineando

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -26,14 +26,14 @@ const WeatherContainer = ({ weather, dark, setDark }) => {
             <Button_slider dark={dark} setDark={setDark} ></Button_slider>
             <h3 className='text-xl font-semibold'>{weather.name + ' '},{' ' + weather.sys.country}</h3>
             <div className='grid gap-5 sm:grid-cols-[1fr_auto]' >
-            <article className="bg-slate-500/50 rounded-[38px] grid grid-cols-2 gap-2 py-6 items-center justify-center ">
+            <article className="bg-slate-500/50 rounded-[38px] grid grid-cols-2 gap-0 py-6 items-center justify-center ">
                 <h4 className='col-span-2 text-2xl capitalize'>{weather.weather[0].description}</h4>
                 <span className='text-4xl' >{changeUnitTemp(weather.main.temp)}</span>
                 <picture className='grid justify-center items-center'>
                     <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt={weather.weather[0].description} />
                 </picture>
             </article>
-            <article className="grid grid-cols-3  divide-x py-4 justify-center items-center bg-slate-500/50 rounded-2xl min-h-max w-full gap-1 sm:grid-cols-1  sm:divide-x-0 sm:divide-y-2">
+            <article className="grid grid-cols-3  divide-x py-4 justify-center items-center bg-slate-500/50 rounded-2xl min-h-max w-full gap-1 sm:grid-cols-1  sm:divide-x-0 sm:divide-y-2 sm:justify-items-start">
                 <WeatherStat icon={"/wind"} unit={" m/s"} value={weather.wind.speed} dark={dark}></WeatherStat>
                 <WeatherStat icon={"/humidity"} unit={" %"} value={weather.main.humidity} dark={dark}></WeatherStat>
                 <WeatherStat icon={"/pressure"} unit={" hPa"} value={weather.main.pressure} dark={dark}></WeatherStat>
--- src/components/WeatherStat.jsx
+++ src/components/WeatherStat.jsx
@@ -3,7 +3,7 @@ const WeatherStat = ({icon, unit, value, dark}) => {
     const iconDark= icon+'_dark.svg'
     const iconLight= icon+'_light.svg'
     return (
-        <div className="flex gap-1 items-center justify-center sm:p-[24px] font-semibold">
+        <div className="flex gap-1 items-center justify-center sm:p-[24px] font-semibold sm:gap-4 sm:min-w-full sm:justify-start">
                 {dark===true?
                 <>
                 <img className="h-5 w-5 " src={iconDark} />
@@ -11,7 +11,7 @@ const WeatherStat = ({icon, unit, value, dark}) => {
                 :
                 <img className="h-5 w-5" src={iconLight} />
                 }
-            <span>{value}{unit}</span>
+            <span >{value}{unit}</span>
         </div>
     )
 }
